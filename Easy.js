65498dfb9bbabc64ee9268374e52dfbca8471397
Zaid <115126069+zaid-saied@users.noreply.github.com>
2022-12-03 22:25:50 -0500

    Add files via upload

--- Easy.js
+++ Easy.js
@@ -12,22 +12,22 @@ function darkMode() {
 
 function color() {
     const r = [];
-    const c = ["c1", "c2", "c3", "c4", "c5"];
+    const c = ["c1", "c2", "c3"];
     var diff = false;
     var j = 0;
 
     while (diff == false) {
-        while (r.length < 15) {
+        while (r.length < 9) {
             var rand = Math.floor(Math.random() * 256);
             if (r.indexOf(rand) === -1) {
                 r.push(rand);
             }
-            if (r[0] != r[1] != r[2] != r[3] != r[4] != r[5] != r[6] != r[7] != r[8] != r[9] != r[10] != r[11] != r[12] != r[13] != r[14]) {
+            if (r[0] != r[1] != r[2] != r[3] != r[4] != r[5] != r[6] != r[7] != r[8]) {
                 diff = true;
             }
         }
     }
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 3; i++) {
         document.getElementById(c[i]).style.backgroundColor = "rgb(" + r[j] + ", " + r[j + 1] + ", " + r[j + 2] + ")";
         j = j + 3;
     }
@@ -45,16 +45,8 @@ function clicked3() {
     clicked = 3;
 }
 
-function clicked4() {
-    clicked = 4;
-}
-
-function clicked5() {
-    clicked = 5;
-}
-
 function correct() {
-    var c = Math.floor(Math.random() * 5) + 1;
+    var c = Math.floor(Math.random() * 3) + 1;
     return c;
 }
 
