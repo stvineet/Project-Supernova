10ab1b62cdc79ede039a7c086feb2d69f3c1f52a
Zaid <115126069+zaid-saied@users.noreply.github.com>
2022-12-04 21:06:14 -0500

    Delete Main.js

--- Main.js
+++
@@ -1,9 +0,0 @@
-var x = document.getElementById("css").href;
-
-function darkMode() {
-    if (x == "MainDark.css") {
-        x = document.getElementById("css").href = "Main.css"
-    } else {
-        x = document.getElementById("css").href = "MainDark.css"
-    }
-}
